3fc5333ecfa27a6e9ed540ec49737890d009b581
Maria Eduarda Mendes <maria.m10@aluno.ifsc.edu.br>
2023-10-25 10:10:20 -0300

    Mudança tamanho - PopuUp Sobre

--- Roteiro01/src/view/JanelaSobre.java
+++ Roteiro01/src/view/JanelaSobre.java
@@ -38,7 +38,7 @@ public class JanelaSobre extends JFrame {
 	 */
 	public JanelaSobre() {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 360, 242);
+		setBounds(100, 100, 280, 244);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
